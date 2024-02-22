9e7107cd22a7bac641897671d7618d5c6eca63a7
KIGARDE <kigardetom2001@gmail.com>
2024-02-22 09:13:19 +0300

    save codes to csv file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let chars =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+const fs = require("fs");
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 function generateCodes(numChars, numCodes) {
+  console.time("generateCodes");
   let codes = [];
   for (let i = 0; i < numCodes; i++) {
     let code = "";
@@ -11,7 +12,30 @@ function generateCodes(numChars, numCodes) {
     }
     codes.push(code);
   }
+  console.timeEnd("generateCodes");
   return codes;
 }
 
-console.log(generateCodes(4, 5));
+function saveCodes(codes, fileName) {
+  console.time("saveCodes");
+  let csv = "codes,";
+
+  codes.forEach((code) => {
+    csv += code + ",";
+  });
+
+  csv = csv.slice(0, -1);
+
+  fs.writeFile(fileName, csv, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`File saved successfully to ${fileName}`);
+    }
+  });
+  console.timeEnd("saveCodes");
+}
+
+let codes = generateCodes(5, 1000000);
+saveCodes(codes, "codes.csv");
+// console.log(codes);
